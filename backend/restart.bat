5fe39d00d94fd07fe4ec93782cbc938716fe848e
Abhishek Awasthi <abhishekawasthi@Abhisheks-MacBook-Air.local>
2026-03-01 01:00:01 +0530

    update

--- backend/restart.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-echo Clearing Laravel cache...
-php artisan config:clear
-php artisan cache:clear
-php artisan route:clear
-
-echo.
-echo Cache cleared! Now restart the server with:
-echo php artisan serve
